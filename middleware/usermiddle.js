0875108c311d33e5abddb36ef6ee520d3ea96c34
thuy13705 <18120587@student.hcmus.edu.vn>
2021-11-15 11:36:52 +0700

    push classes, user login

--- middleware/usermiddle.js
+++ middleware/usermiddle.js
@@ -1,43 +1,43 @@
-const node_validator = require('node-input-validator'),
-      errorHelper    = require('../helpers/errorHelper'),
-      { Validator }  = node_validator
+// const node_validator = require('node-input-validator'),
+//       errorHelper    = require('../helpers/errorHelper'),
+//       { Validator }  = node_validator
 
-let REGISTER = async function( req, res, next ){
+// let REGISTER = async function( req, res, next ){
 
-    let validate = new Validator(req.body, {
-        nameUser: "required|string|minLength:1|maxLength:10000",
-        email   : "required|email|minLength:3|maxLength:500",
-        password: "required|string|minLength:6|maxLength:1000",
-    },{
-        'title.required'     : ":attribute is required"
-    });
+//     let validate = new Validator(req.body, {
+//         nameUser: "required|string|minLength:1|maxLength:10000",
+//         email   : "required|email|minLength:3|maxLength:500",
+//         password: "required|string|minLength:6|maxLength:1000",
+//     },{
+//         'title.required'     : ":attribute is required"
+//     });
      
-    let matched = await validate.check()
-    if (!matched) {
-        req.errors = validate.errors
-        return errorHelper.apiResponseErrorResource( req, res )
-    }
-    next()
-}
+//     let matched = await validate.check()
+//     if (!matched) {
+//         req.errors = validate.errors
+//         return errorHelper.apiResponseErrorResource( req, res )
+//     }
+//     next()
+// }
 
-let LOGIN = async function( req, res, next ){
+// let LOGIN = async function( req, res, next ){
 
-    let validate = new Validator(req.body, {
-        email   : "required|email|minLength:3|maxLength:500",
-        password: "required|string|minLength:6|maxLength:1000",
-    },{
-        'title.required'     : ":attribute is required"
-    });
+//     let validate = new Validator(req.body, {
+//         email   : "required|email|minLength:3|maxLength:500",
+//         password: "required|string|minLength:6|maxLength:1000",
+//     },{
+//         'title.required'     : ":attribute is required"
+//     });
      
-    let matched = await validate.check()
-    if (!matched) {
-        req.errors = validate.errors
-        return errorHelper.apiResponseErrorResource( req, res )
-    }
-    next()
-}
+//     let matched = await validate.check()
+//     if (!matched) {
+//         req.errors = validate.errors
+//         return errorHelper.apiResponseErrorResource( req, res )
+//     }
+//     next()
+// }
 
-module.exports = {
-    REGISTER,
-    LOGIN,
-}
+// module.exports = {
+//     REGISTER,
+//     LOGIN,
+// }
