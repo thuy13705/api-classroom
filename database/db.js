ca19e9776bc07871ef5cd32e27446bc3918b54d7
thuy13705 <48061317+thuy13705@users.noreply.github.com>
2021-11-15 11:41:22 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,6 @@
 var moogoose=require('mongoose');
 
-const uri='mongodb+srv://thuynguyen:thuy13705@thuynguyen.vpkmh.mongodb.net/test?authSource=admin&replicaSet=atlas-zozsyl-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-
+const uri=''
 const con=moogoose.connect(uri,(error)=>{
     if (error){
         console.log("Error"+error);
